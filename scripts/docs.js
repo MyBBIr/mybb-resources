4c15c99b7073f5e9c6bf2dfad76185fd44b9c43d
ATofighi <alirtofighim@gmail.com>
2014-01-16 01:28:51 +0330

    Update docs.js

--- scripts/docs.js
+++ scripts/docs.js
@@ -1,4 +1,4 @@
-jQuery(function($) {
+$(function() {
 	$('.docs_subcat').hide();
 	$('.docs_categories li a').on('click', function(event) {
 		$(event.target).parent().children('.docs_subcat').toggle('fast');
